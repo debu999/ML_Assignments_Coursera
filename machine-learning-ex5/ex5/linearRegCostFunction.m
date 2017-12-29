c3cdcf2899d3c1c1e15d671b4b4bd7fe5668e485
Debabrata Patnaik <debabrata_patnaik@live.com>
2017-12-29 13:40:08 +0800

    Assignments Advice on Machine Learning
    Advice on Machine Learing Assignments Coursera

---
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -0,0 +1,38 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear 
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+    % Debabrata Comments - Coding Start
+    % Write regularized cost function J(x)
+    % theta 2*1 X 12*2 y 12 *1
+        h = X*theta;
+        J = (1/(2*m)) * sum((h-y).^2); % Unregularized Cost Function
+        theta_unbiased = [zeros(1,size(theta,2));theta(2:end,:)];
+        reg_factor = (lambda/(2*m))*sum(theta_unbiased.^2); % reg_factor
+        J = J + reg_factor; % cost = unreg cst fn + reg_factor
+        grad = X'*(h-y)/m; % gradient = X*(h-y)/m
+        grad = grad + (lambda*theta_unbiased)/m; % penalty grad = lambda/m * theta without bias.
+    % Debabrata Comments - Coding End
+    % 
+
+% =========================================================================
+
+grad = grad(:);
+
+end
